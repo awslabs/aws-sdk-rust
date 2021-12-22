01c906602c974a9db4db7b3bb043bf18398b5f45
Zelda Hessler <zhessler@amazon.com>
2021-12-22 14:18:15 -0600

    add: docs

--- tools/smithy-rs-sync/src/main.rs
+++ tools/smithy-rs-sync/src/main.rs
@@ -8,7 +8,8 @@ use std::time::Instant;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
-#[structopt(name = "sync")]
+#[structopt(name = "smithy-rs-sync")]
+/// A CLI tool to replay commits from smithy-rs, generate code, and commit that code to aws-rust-sdk
 struct Opt {
     /// The path to the smithy-rs repo folder
     #[structopt(long, parse(from_os_str))]
@@ -16,7 +17,7 @@ struct Opt {
     /// The path to the aws-sdk-rust folder
     #[structopt(long, parse(from_os_str))]
     aws_sdk: PathBuf,
-    /// (Optional) The branch in aws-sdk-rust that commits will be mirrored to (defaults to 'next')
+    /// The branch in aws-sdk-rust that commits will be mirrored to
     #[structopt(long, default_value = "next")]
     branch: String,
 }
@@ -24,12 +25,19 @@ struct Opt {
 const BOT_NAME: &str = "AWS SDK Rust Bot";
 const BOT_EMAIL: &str = "aws-sdk-rust-primary@amazon.com";
 
-/*
-cargo run -- \
---smithy-rs /Users/zhessler/Documents/smithy-rs-test/ \
---aws-sdk /Users/zhessler/Documents/aws-sdk-rust-test/
-*/
-
+/// Run this app in order to keep aws-sdk-rust in sync with smithy-rs.
+///
+/// pre-requisites:
+/// - an up-to-date local copy of smithy-rs repo
+/// - an up-to-date local copy of aws-sdk-rs repo
+/// - a Unix-ey system (for the `cp` and `rf` commands to work)
+/// - Java Runtime Environment v11 (in order to run gradle commands)
+///
+/// ```sh
+/// cargo run -- \
+/// --smithy-rs /Users/zhessler/Documents/smithy-rs-test/ \
+/// --aws-sdk /Users/zhessler/Documents/aws-sdk-rust-test/
+/// ```
 fn main() {
     let Opt {
         smithy_rs,
@@ -37,12 +45,14 @@ fn main() {
         branch,
     } = Opt::from_args();
 
-    sync_with_latest(&smithy_rs, &aws_sdk, &branch).unwrap();
+    sync_aws_sdk_with_smithy_rs(&smithy_rs, &aws_sdk, &branch).unwrap();
 }
 
-fn sync_with_latest(smithy_rs: &Path, aws_sdk: &Path, branch: &str) -> Result<()> {
+/// Run through all commits made to `smithy-rs` since last sync and "replay" them onto `aws-sdk-rust`
+fn sync_aws_sdk_with_smithy_rs(smithy_rs: &Path, aws_sdk: &Path, branch: &str) -> Result<()> {
     let context = "couldn't sync commits";
 
+    // Open the repositories we'll be working with
     let smithy_rs_repo = Repository::open(smithy_rs).context(context).unwrap();
     let aws_sdk_repo = Repository::open(aws_sdk).context(context).unwrap();
 
@@ -57,12 +67,15 @@ fn sync_with_latest(smithy_rs: &Path, aws_sdk: &Path, branch: &str) -> Result<()
         return Ok(());
     }
 
+    // `git checkout` the branch of `aws-sdk-rust` that we want to replay commits onto.
+    // By default, this is the `next` branch.
     checkout_branch_to_sync_to(&aws_sdk_repo, branch)
         .context(context)
         .unwrap();
 
     let number_of_commits = commit_revs.len();
     println!("Syncing {} commit(s)...", number_of_commits);
+    // Run through all the new commits, syncing them one by one
     for (i, rev) in commit_revs.iter().enumerate() {
         let commit = smithy_rs_repo.find_commit(*rev).context(context).unwrap();
 
@@ -72,14 +85,19 @@ fn sync_with_latest(smithy_rs: &Path, aws_sdk: &Path, branch: &str) -> Result<()
             .context(context)
             .unwrap();
     }
-    println!("Successfully synced {} commits", commit_revs.len());
+    println!("Successfully synced {} commit(s)", commit_revs.len());
 
-    // TODO come up with better name
+    // Get the last commit we synced so that we can set that for the next time this tool gets run
     let last_synced_commit = commit_revs
         .last()
         .expect("can't be empty because we'd have early returned");
     println!("updating 'commit at last sync' to {}", last_synced_commit);
-    // set_last_synced_commit(aws_sdk, last_synced_commit).context(context).unwrap();
+
+    // Update the file containing the commit hash
+    set_last_synced_commit(&aws_sdk_repo, last_synced_commit)
+        .context(context)
+        .unwrap();
+    // Commit the file containg the commit hash
     commit_last_synced_commit_file(&aws_sdk_repo)
         .context(context)
         .unwrap();
@@ -89,8 +107,10 @@ fn sync_with_latest(smithy_rs: &Path, aws_sdk: &Path, branch: &str) -> Result<()
     Ok(())
 }
 
-// If you don't see the commits you're expecting, make sure the repo is up to date
-// This function doesn't include the `since_commit` in the list
+/// Starting from a given commit, walk the tree to its `HEAD` in order to build a list of commits that we'll
+/// need to sync. If you don't see the commits you're expecting, make sure the repo is up to date.
+/// This function doesn't include the `since_commit` in the list since that commit was synced last time
+/// this tool was run.
 fn commits_to_be_applied(smithy_rs_repo: &Repository, since_commit: &Oid) -> Result<Vec<Oid>> {
     let context = "couldn't create list of commits to be applied";
     let mut commit_revs = Vec::new();
@@ -114,6 +134,8 @@ fn commits_to_be_applied(smithy_rs_repo: &Repository, since_commit: &Oid) -> Res
 
 const COMMIT_HASH_FILENAME: &str = ".smithyrs-githash";
 
+/// Read the file from aws-sdk-rust that tracks the last smithy-rs commit it was synced with.
+/// Returns the hash of that commit.
 fn get_last_synced_commit(repo_path: &Path) -> Result<Oid> {
     let path = repo_path.join(COMMIT_HASH_FILENAME);
 
@@ -135,7 +157,9 @@ fn get_last_synced_commit(repo_path: &Path) -> Result<Oid> {
     Ok(oid)
 }
 
-fn _set_last_synced_commit(repo_path: &Path, oid: Oid) -> Result<()> {
+/// Write the last synced commit to the file in aws-sdk-rust that tracks the last smithy-rs commit it was synced with.
+fn set_last_synced_commit(repo: &Repository, oid: &Oid) -> Result<()> {
+    let repo_path = repo.workdir().expect("this will always exist");
     let path = repo_path.join(COMMIT_HASH_FILENAME);
     let mut file = OpenOptions::new()
         .write(true)
@@ -150,6 +174,8 @@ fn _set_last_synced_commit(repo_path: &Path, oid: Oid) -> Result<()> {
     Ok(())
 }
 
+/// Run the gradle commands to build a fresh SDK in smithy-rs, clean out old SDK files from aws-sdk-rust,
+/// copy over the freshly built SDK, and then commit those changes.
 fn build_copy_and_commit_sdk(
     commit_to_mirror: &Commit,
     aws_sdk_repo: &Repository,
@@ -166,8 +192,8 @@ fn build_copy_and_commit_sdk(
     Ok(())
 }
 
-/// Run the necessary commands to build the SDK, returning the path to the folder containing
-/// the build artifacts on success.
+/// Run the necessary commands to build the SDK. On success, returns the path to the folder containing
+/// the build artifacts.
 fn build_sdk(smithy_rs_path: &Path) -> Result<PathBuf> {
     println!("\tbuilding the SDK...");
     let start = Instant::now();
@@ -226,6 +252,7 @@ fn build_sdk(smithy_rs_path: &Path) -> Result<PathBuf> {
     Ok(build_artifact_path)
 }
 
+/// Delete any current SDK files in aws-sdk-rust. Run this before copying over new files.
 fn clean_out_existing_sdk(aws_sdk_path: &Path) -> Result<()> {
     println!("\tcleaning out previously built SDK...");
 
@@ -258,6 +285,8 @@ fn clean_out_existing_sdk(aws_sdk_path: &Path) -> Result<()> {
     Ok(())
 }
 
+/// Use `cp -r` to recursively copy all files and folders from the smithy-rs build artifacts folder
+/// to the aws-sdk-rust repo folder
 fn copy_sdk(from_path: &Path, to_path: &Path) -> Result<()> {
     println!("\tcopying built SDK...");
 
@@ -279,6 +308,7 @@ fn copy_sdk(from_path: &Path, to_path: &Path) -> Result<()> {
     Ok(())
 }
 
+/// Find the last commit made to a repo
 fn find_last_commit(repo: &Repository) -> Result<Commit> {
     let context = "couldn't find last commit";
     let obj = repo
@@ -291,10 +321,11 @@ fn find_last_commit(repo: &Repository) -> Result<Commit> {
         .peel(ObjectType::Commit)
         .context(context)
         .unwrap();
-    obj.into_commit()
-        .map_err(|_| anyhow!("couldn't find last commit"))
+    obj.into_commit().map_err(|_| anyhow!(context))
 }
 
+/// Create a "mirror" commit. Works by reading a smithy-rs commit and then using the info
+/// attached to it to create a commit in aws-sdk-rust.
 fn create_mirror_commit(aws_sdk_repo: &Repository, based_on_commit: &Commit) -> Result<()> {
     println!("\tcreating mirror commit...");
     let context = "couldn't create mirror commit";
@@ -327,6 +358,7 @@ fn create_mirror_commit(aws_sdk_repo: &Repository, based_on_commit: &Commit) -> 
     Ok(())
 }
 
+/// Commit the file in aws-sdk-rust that tracks what smithy-rs commit the SDK was last built from
 fn commit_last_synced_commit_file(aws_sdk_repo: &Repository) -> Result<()> {
     let context = format!("couldn't commit the '{}' file", COMMIT_HASH_FILENAME);
 
@@ -363,9 +395,10 @@ fn commit_last_synced_commit_file(aws_sdk_repo: &Repository) -> Result<()> {
     Ok(())
 }
 
+/// `git checkout` the branch of aws-sdk-rust that we want to mirror commits to (defaults to `next`)
 fn checkout_branch_to_sync_to(aws_sdk_repo: &Repository, aws_sdk_branch: &str) -> Result<()> {
     let context = "checking out branch to sync to";
-    // TODO don't need to fetch in CI, delete this
+
     aws_sdk_repo
         .find_remote("origin")?
         .fetch(&["main"], None, None)
@@ -383,6 +416,7 @@ fn checkout_branch_to_sync_to(aws_sdk_repo: &Repository, aws_sdk_branch: &str) -
     Ok(())
 }
 
+/// `git checkout` a commit from smithy-rs that we're going to mirror to aws-sdk-rust
 fn checkout_commit_to_sync_from(smithy_rs_repo: &Repository, commit: &Commit) -> Result<()> {
     let head = smithy_rs_repo.head().unwrap().target().unwrap();
     let head = smithy_rs_repo.find_object(head, None).unwrap();
