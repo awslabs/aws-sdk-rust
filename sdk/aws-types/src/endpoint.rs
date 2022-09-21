9f66a955eec13f60aaaea98447864eb958ef9a7b
Zelda Hessler <zhessler@amazon.com>
2022-09-21 00:16:37 +0000

    [smithy-rs] remove: outdated TODO regarding endpoint errors (#1721)

--- sdk/aws-types/src/endpoint.rs
+++ sdk/aws-types/src/endpoint.rs
@@ -81,7 +81,6 @@ pub type BoxError = Box<dyn Error + Send + Sync + 'static>;
 /// Each AWS service generates their own implementation of `ResolveAwsEndpoint`.
 pub trait ResolveAwsEndpoint: Send + Sync + Debug {
     /// Resolves the AWS endpoint for a given region.
-    // TODO(https://github.com/awslabs/smithy-rs/issues/866): Create `ResolveEndpointError`
     fn resolve_endpoint(&self, region: &Region) -> Result<AwsEndpoint, BoxError>;
 }
 
