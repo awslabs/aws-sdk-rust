26d060b50233ddbf30103dd965036bb564b8fc35
Jesus Cova <46403187+jesuscovam@users.noreply.github.com>
2022-10-12 20:04:52 -0500

    fix "#600"

--- sdk/s3/src/lib.rs
+++ sdk/s3/src/lib.rs
@@ -72,6 +72,7 @@ pub static PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
 pub mod types {
     pub use aws_smithy_http::byte_stream::AggregatedBytes;
     pub use aws_smithy_http::byte_stream::ByteStream;
+    pub use aws_smithy_http::byte_stream::Error as ByteStreamError;
     pub use aws_smithy_http::result::SdkError;
     pub use aws_smithy_types::Blob;
     pub use aws_smithy_types::DateTime;
