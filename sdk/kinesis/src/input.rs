486e7e16c5ef97128a4bcf1609b716ed59272963
82marbag <69267416+82marbag@users.noreply.github.com>
2023-01-13 21:43:38 +0000

    [smithy-rs] Request IDs (#2054)
    * Request IDs
    
    Signed-off-by: Daniele Ahmed <ahmeddan@amazon.de>

--- sdk/kinesis/src/input.rs
+++ sdk/kinesis/src/input.rs
@@ -842,8 +842,8 @@ impl DeregisterStreamConsumerInput {
             .set_region(_config.region.as_ref().map(|r| r.as_ref().to_owned()))
             .set_endpoint(_config.endpoint_url().map(|url| url.to_string()))
             .set_operation_type(Some("control".to_string()))
-            .set_consumer_arn(self.consumer_arn.clone())
             .set_stream_arn(self.stream_arn.clone())
+            .set_consumer_arn(self.consumer_arn.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
@@ -1380,8 +1380,8 @@ impl DescribeStreamConsumerInput {
             .set_region(_config.region.as_ref().map(|r| r.as_ref().to_owned()))
             .set_endpoint(_config.endpoint_url().map(|url| url.to_string()))
             .set_operation_type(Some("control".to_string()))
-            .set_consumer_arn(self.consumer_arn.clone())
             .set_stream_arn(self.stream_arn.clone())
+            .set_consumer_arn(self.consumer_arn.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
