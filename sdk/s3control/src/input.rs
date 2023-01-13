b04e23d70053e2ec140e0b6f8a059d9c263b4899
John DiSanti <jdisanti@amazon.com>
2023-01-13 21:44:27 +0000

    [smithy-rs] Fix Lambda bundle naming for the SDK canary (#2135)
    * Add release tag and Rust version to canary bundle file name
      Update canary CDK dependencies
      Update Lambda name in logging permissions

--- sdk/s3control/src/input.rs
+++ sdk/s3control/src/input.rs
@@ -5706,8 +5706,8 @@ impl GetBucketInput {
             .set_endpoint(_config.endpoint_url().map(|url| url.to_string()))
             .set_use_arn_region(_config.use_arn_region)
             .set_requires_account_id(Some(true))
-            .set_account_id(self.account_id.clone())
             .set_bucket(self.bucket.clone())
+            .set_account_id(self.account_id.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
@@ -5909,8 +5909,8 @@ impl GetBucketLifecycleConfigurationInput {
             .set_endpoint(_config.endpoint_url().map(|url| url.to_string()))
             .set_use_arn_region(_config.use_arn_region)
             .set_requires_account_id(Some(true))
-            .set_bucket(self.bucket.clone())
             .set_account_id(self.account_id.clone())
+            .set_bucket(self.bucket.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
@@ -8234,8 +8234,8 @@ impl ListAccessPointsInput {
             .set_endpoint(_config.endpoint_url().map(|url| url.to_string()))
             .set_use_arn_region(_config.use_arn_region)
             .set_requires_account_id(Some(true))
-            .set_account_id(self.account_id.clone())
             .set_bucket(self.bucket.clone())
+            .set_account_id(self.account_id.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
@@ -10374,8 +10374,8 @@ impl PutBucketPolicyInput {
             .set_endpoint(_config.endpoint_url().map(|url| url.to_string()))
             .set_use_arn_region(_config.use_arn_region)
             .set_requires_account_id(Some(true))
-            .set_bucket(self.bucket.clone())
             .set_account_id(self.account_id.clone())
+            .set_bucket(self.bucket.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
