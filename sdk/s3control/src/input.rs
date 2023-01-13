228ec6fee8625900f1e0812d1651f922176547c6
Thomas Cameron <68596478+thomas-k-cameron@users.noreply.github.com>
2023-01-13 21:47:40 +0000

    [smithy-rs] UPDATE for RFC30: Serialization and deserialization (#2186)
    * commit RFC
    
    * commit
    
    * Delete rfc0023_serialization_and_deserialization.md
    
    * fix formatting
    
    * https://github.com/awslabs/smithy-rs/pull/1944#discussion_r1054637168
    
    * https://github.com/awslabs/smithy-rs/pull/1944#discussion_r1054632202
    
    * applied grammarly
    pre-commit
    
    * better example wip
    
    * FIX
    
    * https://github.com/awslabs/smithy-rs/pull/1944#discussion_r1054632468
    
    https://github.com/awslabs/smithy-rs/pull/1944#discussion_r1054818444
    
    https://github.com/awslabs/smithy-rs/pull/1944#discussion_r1054820192
    
    * add examples for output's builer
    
    * fixes
    
    * fixing unstable feature gate snippet
    
    * Update rfc0028_serialization_and_deserialization.md
    
    Section is deleted in response to the discussion
    https://github.com/awslabs/smithy-rs/pull/1944#discussion_r1061577656
    
    * file name fix
    
    * Update rfc0030_serialization_and_deserialization.md
    
    https://github.com/awslabs/smithy-rs/pull/2183#discussion_r1064573442
    
    * Update rfc0030_serialization_and_deserialization.md

--- sdk/s3control/src/input.rs
+++ sdk/s3control/src/input.rs
@@ -2472,8 +2472,8 @@ impl DeleteBucketLifecycleConfigurationInput {
             .set_use_fips(_config.use_fips)
             .set_use_dual_stack(_config.use_dual_stack)
             .set_requires_account_id(Some(true))
-            .set_bucket(self.bucket.clone())
             .set_account_id(self.account_id.clone())
+            .set_bucket(self.bucket.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
@@ -4993,8 +4993,8 @@ impl GetAccessPointPolicyInput {
             .set_use_fips(_config.use_fips)
             .set_use_dual_stack(_config.use_dual_stack)
             .set_requires_account_id(Some(true))
-            .set_access_point_name(self.name.clone())
             .set_account_id(self.account_id.clone())
+            .set_access_point_name(self.name.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
@@ -10207,8 +10207,8 @@ impl PutBucketLifecycleConfigurationInput {
             .set_use_fips(_config.use_fips)
             .set_use_dual_stack(_config.use_dual_stack)
             .set_requires_account_id(Some(true))
-            .set_account_id(self.account_id.clone())
             .set_bucket(self.bucket.clone())
+            .set_account_id(self.account_id.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
