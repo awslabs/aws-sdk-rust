649336e0ef6565ba567df6c7ce54fb87506a625a
John DiSanti <jdisanti@amazon.com>
2023-03-28 12:24:13 -0400

    [smithy-rs] Move symbol extension functions into `SymbolExt` (#2378)

--- sdk/s3control/src/input.rs
+++ sdk/s3control/src/input.rs
@@ -157,8 +157,8 @@ impl CreateAccessPointInput {
             .set_endpoint(_config.endpoint_url.clone())
             .set_use_arn_region(_config.use_arn_region)
             .set_requires_account_id(Some(true))
-            .set_account_id(self.account_id.clone())
             .set_bucket(self.bucket.clone())
+            .set_account_id(self.account_id.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
@@ -5793,8 +5793,8 @@ impl GetBucketInput {
             .set_endpoint(_config.endpoint_url.clone())
             .set_use_arn_region(_config.use_arn_region)
             .set_requires_account_id(Some(true))
-            .set_bucket(self.bucket.clone())
             .set_account_id(self.account_id.clone())
+            .set_bucket(self.bucket.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
@@ -9821,8 +9821,8 @@ impl PutAccessPointPolicyInput {
             .set_endpoint(_config.endpoint_url.clone())
             .set_use_arn_region(_config.use_arn_region)
             .set_requires_account_id(Some(true))
-            .set_account_id(self.account_id.clone())
             .set_access_point_name(self.name.clone())
+            .set_account_id(self.account_id.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
