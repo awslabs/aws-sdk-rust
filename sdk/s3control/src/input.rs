bd1ac39821d8616617bc52f7497ef12e36fbe2c6
david-perez <d@vidp.dev>
2023-03-28 12:23:54 -0400

    [smithy-rs] Allow server decorators to postprocess `ValidationException` not attached error messages (#2338)
    Should they want to, a server decorator can now postprocess the error
    message that arises when a constrained operation does not have the
    `ValidationException` shape attached to its errors.
    
    This commit adds a test to ensure that when such a decorator is
    registered, the `ValidationResult` can indeed be altered, but no such
    decorator is added to the `rust-server-codegen` plugin.

--- sdk/s3control/src/input.rs
+++ sdk/s3control/src/input.rs
@@ -157,8 +157,8 @@ impl CreateAccessPointInput {
             .set_endpoint(_config.endpoint_url.clone())
             .set_use_arn_region(_config.use_arn_region)
             .set_requires_account_id(Some(true))
-            .set_bucket(self.bucket.clone())
             .set_account_id(self.account_id.clone())
+            .set_bucket(self.bucket.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
@@ -726,8 +726,8 @@ impl CreateBucketInput {
             .set_use_dual_stack(_config.use_dual_stack)
             .set_endpoint(_config.endpoint_url.clone())
             .set_use_arn_region(_config.use_arn_region)
-            .set_outpost_id(self.outpost_id.clone())
             .set_bucket(self.bucket.clone())
+            .set_outpost_id(self.outpost_id.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
@@ -5999,8 +5999,8 @@ impl GetBucketLifecycleConfigurationInput {
             .set_endpoint(_config.endpoint_url.clone())
             .set_use_arn_region(_config.use_arn_region)
             .set_requires_account_id(Some(true))
-            .set_bucket(self.bucket.clone())
             .set_account_id(self.account_id.clone())
+            .set_bucket(self.bucket.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
@@ -6211,8 +6211,8 @@ impl GetBucketPolicyInput {
             .set_endpoint(_config.endpoint_url.clone())
             .set_use_arn_region(_config.use_arn_region)
             .set_requires_account_id(Some(true))
-            .set_account_id(self.account_id.clone())
             .set_bucket(self.bucket.clone())
+            .set_account_id(self.account_id.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
@@ -8360,8 +8360,8 @@ impl ListAccessPointsInput {
             .set_endpoint(_config.endpoint_url.clone())
             .set_use_arn_region(_config.use_arn_region)
             .set_requires_account_id(Some(true))
-            .set_account_id(self.account_id.clone())
             .set_bucket(self.bucket.clone())
+            .set_account_id(self.account_id.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
@@ -10532,8 +10532,8 @@ impl PutBucketPolicyInput {
             .set_endpoint(_config.endpoint_url.clone())
             .set_use_arn_region(_config.use_arn_region)
             .set_requires_account_id(Some(true))
-            .set_bucket(self.bucket.clone())
             .set_account_id(self.account_id.clone())
+            .set_bucket(self.bucket.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
