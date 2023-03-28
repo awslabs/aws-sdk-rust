f0fd14457490a64b72e00ee7cfd3844f30786d1a
ysaito1001 <gperson22@gmail.com>
2023-03-28 14:30:37 -0400

    [smithy-rs] Fix Inaccurate documentation on ProfileFileCredentialsProvider (#2421)
    This commit addresses https://github.com/awslabs/aws-sdk-rust/issues/746.
    
    Co-authored-by: Yuki Saito <awsaito@amazon.com>

--- sdk/s3control/src/input.rs
+++ sdk/s3control/src/input.rs
@@ -324,8 +324,8 @@ impl CreateBucketInput {
             .set_use_dual_stack(_config.use_dual_stack)
             .set_endpoint(_config.endpoint_url.clone())
             .set_use_arn_region(_config.use_arn_region)
-            .set_bucket(self.bucket.clone())
             .set_outpost_id(self.outpost_id.clone())
+            .set_bucket(self.bucket.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
@@ -2836,8 +2836,8 @@ impl GetAccessPointInput {
             .set_endpoint(_config.endpoint_url.clone())
             .set_use_arn_region(_config.use_arn_region)
             .set_requires_account_id(Some(true))
-            .set_access_point_name(self.name.clone())
             .set_account_id(self.account_id.clone())
+            .set_access_point_name(self.name.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
@@ -3249,8 +3249,8 @@ impl GetAccessPointPolicyInput {
             .set_endpoint(_config.endpoint_url.clone())
             .set_use_arn_region(_config.use_arn_region)
             .set_requires_account_id(Some(true))
-            .set_access_point_name(self.name.clone())
             .set_account_id(self.account_id.clone())
+            .set_access_point_name(self.name.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
@@ -3526,8 +3526,8 @@ impl GetAccessPointPolicyStatusInput {
             .set_endpoint(_config.endpoint_url.clone())
             .set_use_arn_region(_config.use_arn_region)
             .set_requires_account_id(Some(true))
-            .set_account_id(self.account_id.clone())
             .set_access_point_name(self.name.clone())
+            .set_account_id(self.account_id.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
@@ -3937,8 +3937,8 @@ impl GetBucketLifecycleConfigurationInput {
             .set_endpoint(_config.endpoint_url.clone())
             .set_use_arn_region(_config.use_arn_region)
             .set_requires_account_id(Some(true))
-            .set_bucket(self.bucket.clone())
             .set_account_id(self.account_id.clone())
+            .set_bucket(self.bucket.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
@@ -4213,8 +4213,8 @@ impl GetBucketTaggingInput {
             .set_endpoint(_config.endpoint_url.clone())
             .set_use_arn_region(_config.use_arn_region)
             .set_requires_account_id(Some(true))
-            .set_bucket(self.bucket.clone())
             .set_account_id(self.account_id.clone())
+            .set_bucket(self.bucket.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
@@ -6196,8 +6196,8 @@ impl ListRegionalBucketsInput {
             .set_endpoint(_config.endpoint_url.clone())
             .set_use_arn_region(_config.use_arn_region)
             .set_requires_account_id(Some(true))
-            .set_outpost_id(self.outpost_id.clone())
             .set_account_id(self.account_id.clone())
+            .set_outpost_id(self.outpost_id.clone())
             .build()
             .map_err(|err| {
                 aws_smithy_http::endpoint::ResolveEndpointError::from_source(
