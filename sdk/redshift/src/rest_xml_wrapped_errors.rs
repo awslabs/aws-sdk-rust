222f8db1cb3cd456057abf10c5deae3e6f5be09f
Julian Antonielli <julianantonielli@gmail.com>
2023-03-30 17:51:20 +0000

    [smithy-rs] Refactor event stream tests with `{client,server}IntegrationTest`s (#2342)
    * Refactor `ClientEventStreamUnmarshallerGeneratorTest` to use `clientIntegrationTest` (WIP)
    
    * Refactor `ClientEventStreamUnmarshallerGeneratorTest` with `clientIntegrationTest`
    
    * Refactor `ClientEventStreamUnmarshallerGeneratorTest` to use generic test cases
    
    * Start refactoring `ServerEventStreamUnmarshallerGeneratorTest`
    
    * Make `ServerEventStreamUnmarshallerGeneratorTest` tests work
    
    * Uncomment other test models
    
    * Allow unused on `parse_generic_error`
    
    * Rename `ServerEventStreamUnmarshallerGeneratorTest`
    
    * Make `EventStreamUnmarshallTestCases` codegenTarget-agnostic
    
    * Refactor `ClientEventStreamMarshallerGeneratorTest`: Tests run but fail
    
    * Refactor `ServerEventStreamMarshallerGeneratorTest`
    
    * Move `.into()` calls to `conditionalBuilderInput`
    
    * Add "context" to TODO
    
    * Fix client unmarshall tests
    
    * Fix clippy lint
    
    * Fix more clippy lints
    
    * Add docs for `event_stream_serde` module
    
    * Fix client tests
    
    * Remove `#[allow(missing_docs)]` from event stream module
    
    * Remove unused `EventStreamTestTools`
    
    * Add `smithy-validation-model` test dep to `codegen-client`
    
    * Temporarily add docs to make tests compile
    
    * Undo change in model
    
    * Make event stream unmarshaller tests a unit test
    
    * Remove unused code
    
    * Make `ServerEventStreamUnmarshallerGeneratorTest` a unit test
    
    * Make `ServerEventStreamMarshallerGeneratorTest` a unit test
    
    * Make `ServerEventStreamMarshallerGeneratorTest` pass
    
    * Make remaining tests non-integration tests
    
    * Make event stream serde module private again
    
    * Remove unnecessary clippy allowances
    
    * Remove clippy allowance
    
    * Remove docs for `event_stream_serde` module
    
    * Remove docs for `$unmarshallerTypeName::new`
    
    * Remove more unnecessary docs
    
    * Remove more superfluous docs
    
    * Undo unnecessary diffs
    
    * Uncomment last test
    
    * Make `conditionalBuilderInput` internal

--- sdk/redshift/src/rest_xml_wrapped_errors.rs
+++ sdk/redshift/src/rest_xml_wrapped_errors.rs
@@ -15,6 +15,7 @@ pub fn body_is_error(body: &[u8]) -> Result<bool, XmlDecodeError> {
     Ok(scoped.start_el().matches("ErrorResponse"))
 }
 
+#[allow(dead_code)]
 pub fn parse_error_metadata(body: &[u8]) -> Result<ErrorMetadataBuilder, XmlDecodeError> {
     let mut doc = Document::try_from(body)?;
     let mut root = doc.root_element()?;
