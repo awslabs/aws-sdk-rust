58c378d85419e6875247b9a656e70d54282e1089
ysaito1001 <gperson22@gmail.com>
2023-03-30 17:53:42 +0000

    [smithy-rs] Add more client re-exports (#2437)
    * Add more client re-exports
    
    This commit adds more client re-exports proposed in
    https://github.com/awslabs/smithy-rs/issues/1759.
    Specifically, it re-exports the following types:
    - `aws_smithy_http::body::SdkBody`
    - `aws_smithy_http::byte_stream::error::Error`
    - `aws_smithy_http::operation::{Request, Response}`
    
    * Update CHANGELOG.next.toml
    
    ---------
    
    Co-authored-by: Yuki Saito <awsaito@amazon.com>

--- sdk/ebs/src/types.rs
+++ sdk/ebs/src/types.rs
@@ -5,6 +5,8 @@ pub use aws_smithy_types::Blob;
 pub use aws_smithy_types::DateTime;
 pub use aws_smithy_http::byte_stream::ByteStream;
 pub use aws_smithy_http::byte_stream::AggregatedBytes;
+pub use aws_smithy_http::byte_stream::error::Error as ByteStreamError;
+pub use aws_smithy_http::body::SdkBody;
 pub use aws_smithy_http::result::SdkError;
 pub use aws_smithy_types::error::display::DisplayErrorContext;
 pub use aws_smithy_types::error::metadata::ProvideErrorMetadata;
