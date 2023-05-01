db448d8811a4e01994ead0ae06e2bbe4804df4ae
John DiSanti <jdisanti@amazon.com>
2023-05-01 16:02:39 +0000

    [smithy-rs] Remove `enableNewCrateOrganizationScheme` codegen flag (#2507)

--- sdk/s3control/src/operation/list_regional_buckets.rs
+++ sdk/s3control/src/operation/list_regional_buckets.rs
@@ -14,8 +14,8 @@ impl ListRegionalBucketsInput {
         .clone())
         .set_use_arn_region(_config.use_arn_region)
         .set_requires_account_id(Some(true))
-        .set_outpost_id(self.outpost_id.clone())
-        .set_account_id(self.account_id.clone()).build()
+        .set_account_id(self.account_id.clone())
+        .set_outpost_id(self.outpost_id.clone()).build()
                                     .map_err(|err|aws_smithy_http::endpoint::ResolveEndpointError::from_source("could not construct endpoint parameters", err));
                                 let (endpoint_result, params) = match params_result {
                                     Ok(params) => (_config.endpoint_resolver.resolve_endpoint(&params), Some(params)),
