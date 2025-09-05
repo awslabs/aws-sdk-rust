ef5a0a97024b7ec08fa4b2e57c86698998e19d42
ysaito1001 <awsaito@amazon.com>
2025-09-05 15:52:43 -0500

    Remove `examples`

--- examples/cross_service/photo_asset_management/src/handlers/mod.rs
+++
@@ -1,7 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-pub mod detect_labels;
-pub mod download;
-pub mod hello;
-pub mod labels;
-pub mod upload;
--- examples/webassembly/www/env/index.js
+++
@@ -1,8 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-export const now = () => {
-  return Date.now();
-};
-
-export * from "./credentials.js";
