9b1db583368e79ec564f2c408dfbe1cb2d25fef0
ysaito1001 <awsaito@amazon.com>
2025-09-09 10:13:12 -0500

    Remove the `examples` directory (#1351)
    This PR removes the
    [`examples`](https://github.com/awslabs/aws-sdk-rust/tree/67af42f0f7dcf8410782aed5c4d061d71eb0eab9/examples)
    directory from the repository. Going forward, we will direct users to
    [aws-doc-sdk-examples](https://github.com/awsdocs/aws-doc-sdk-examples/tree/main/rustv1)
    in the `README`.
    
    Note: The re-direction will be handled in a subsequent PR. In the
    meantime, the example links in the `README` will be temporarily broken
    after this PR is merged.
    
    ---
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.

--- examples/cross_service/photo_asset_management/src/lib.rs
+++
@@ -1,6 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-pub mod chunked_uploader;
-pub mod common;
-pub mod handlers;
-pub mod uploader;
